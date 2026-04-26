dbb7e6ebb16ab51e139887cb02dd5dcab99d5ad0
ClintDhan <dhaniellerono@gmail.com>
2026-04-26 15:04:56 +0800

    fixed masking algo

--- javascript.js
+++ javascript.js
@@ -1,12 +1,17 @@
-const email = "king@yahoo.com";
+const email = "killua@google.com";
 
 
 const maskEmail = email => {
-    const amper = email.indexOf("@");
-    const address = email.slice(amper, amper.length);
-    const mask = email.slice(1 ,amper-1);
-    const asterisk = email.replace(mask , "*".repeat(mask.length));
-    return asterisk;
+    if(email.includes("@") == false || email.includes("google.com") == false) {
+        return `Email should be google and have a "@". Please try again`;
+    }
+    else {
+        const amper = email.indexOf("@");
+        const firstIndex = email[0];
+        const domain = email.slice(amper);
+        const mask = "*".repeat(amper - 1);
+        return firstIndex + mask + domain;
+    }
 }
 
 console.log(maskEmail(email));
